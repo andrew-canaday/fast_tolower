00a09facbdd6319ff8c3dfd34883554a6f42d938
andrew-canaday <andrew.canaday@gmail.com>
2015-04-08 16:02:55 -0400

    Tweaking + license.

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 # Simple makefile for fast_tolower algorithm test.
-
 benchmark_tolower: main.c
 	gcc ./main.c -o benchmark_tolower
+
+benchmark: benchmark_tolower
+	./benchmark_tolower
 
 clean:
 	rm -vf *.o ./benchmark_tolower
--- fast_tolower.h
+++ fast_tolower.h
@@ -1,7 +1,32 @@
 /*===========================================================================*
  *
  * fast_tolower.h: A neat trick for converting entire strings to lower case
- *                 with minimal branching and CPU.
+ *                 with minimal branching and CPU usage.
+ *
+ * LICENSE:
+ * --------
+ *
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Andrew T. Canaday
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  *
  *
  * Background: Fast Single-Character Conversions:
@@ -14,7 +39,8 @@
  *  - subtract the character value, c, from HIGH
  *  - XOR the difference
  *
- *  (Disclaimer: was first made aware of this here: http://codegolf.stackexchange.com/questions/36425/convert-to-uppercase-and-lowercase-without-branching-and-comparisons)
+ *  (Disclaimer: first made aware of this trick here:
+ *  http://codegolf.stackexchange.com/questions/36425/convert-to-uppercase-and-lowercase-without-branching-and-comparisons)
  *
  * After performing this trick, we have the property that the top bit of the
  * resulting number will be set if LOW <= c <= HIGH (other bits may *also* be
